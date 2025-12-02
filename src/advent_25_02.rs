4a3cefdeaeabf9cd8fc63fdfde880a2425710381
cerman <jaroslav.cerman@msd.com>
2025-12-02 08:26:54 +0100

    Day2 solutions

--- src/advent_25_02.rs
+++ src/advent_25_02.rs
@@ -4,80 +4,115 @@ use crate::*;
 
 // https://adventofcode.com/2025/day/2
 
-
-pub fn main_1() -> i32 {
-    let lines = {
-        open("day01/test_input.txt", "r").readlines()
-    };
-    let lines_len = lines.len();
-    // preallocation, but in the end we did not need the full vec, even in part2
-    let mut directions = vec!["R".to_string(); lines_len];
-    let mut increments = vec![0; lines_len];
-    let mut current = 50;
-    let mut total = 0;
-    let L = "L".to_string();
-    let R = "R".to_string();
-    for (count, line_iter) in enumerate(&lines) {
-        let line = pstr::rstrip(&line_iter);
-        let line_chars = line.chars().collect::<Vec<_>>();
-        directions[count] = line_chars[0].to_string();
-        increments[count] = int(&(line_chars[1..].iter().collect::<String>()));
-        //match &directions[count] {
-        if directions[count] == R {
-            current += increments[count];
-        } else if directions[count] == L {
-            current -= increments[count];
-        } else {
-            panic!("Unknown direction");
-        }
-        current = current % 100;
-        if current == 0 {
-            total += 1
-        }
+/*
+pub fn main_1() -> u64 {
+    // This would solve ony endpoints of intervals
+    {
+        open("day02/test_input.txt", "r")
+        .readlines()[0]
+        .split(",")
+        .map(
+            |x| x.split("-")  // pstr::split(x, "-")
+            .collect::<Vec<&str>>().into_iter()
+            // or lstrip 0
+            .map(|x| int::<u64>(x).to_string())
+            .collect::<Vec<_>>()
+        )
+        .collect::<Vec<_>>().into_iter()
+        .map(|pair| {
+            let a = &pair[0];  //.clone();
+            let b = &pair[1];
+            // no need to check for even len; might speed it up though
+            // working with bytes would also be faster, numbers have guaranteed ASCII
+            let mut subtotal = 0;
+            for c in &[a, b] {
+                let half = c.chars().count() / 2;
+                let c_chars_vec = c.chars().collect::<Vec<_>>();
+                print(&c);
+                if c_chars_vec[..half] == c_chars_vec[half..] {
+                    // I've already made this conversion upper; could store the result
+                    subtotal += int::<u64>(&c);
+                }
+            }
+            subtotal
+        })
+        .collect::<Vec<_>>().iter()
+        .sum()
     }
+}
+*/
 
-    total
+
+pub fn main_1() -> u64 {
+    // bruteforce, fancy oneliner, but not satisfied with all the collect<Vec> calls
+    // solvable by RegEx instead
+    {
+        open("day02/input.txt", "r")
+        .readlines()[0]
+        .split(",")
+        .map(
+            |x| x.split("-")
+            .collect::<Vec<&str>>().into_iter()
+            // or lstrip 0
+            .map(int::<u64>)
+            .collect::<Vec<_>>()
+        )
+        .collect::<Vec<_>>().into_iter()
+        .map(|pair| {
+            // no need to check for even len; might speed it up though
+            // working with bytes would also be faster, numbers have guaranteed ASCII
+            let mut subtotal = 0;
+            for c in pair[0]..pair[1] + 1 {
+                let c_string = c.to_string();
+                let c_chars = c_string.chars();
+                let half = c_chars.clone().count() / 2;
+                let c_chars_vec = c_chars.collect::<Vec<_>>();
+                if c_chars_vec[..half] == c_chars_vec[half..] {
+                    subtotal += c;
+                }
+            }
+            subtotal
+        })
+        .collect::<Vec<_>>().iter()
+        .sum()
+    }
 }
 
 
-pub fn main_2() -> i32 {
-    let lines = {
-        open("day01/input.txt", "r").readlines()
-    };
-    let mut current = 50;
-    let mut total = 0;
-    let L = "L".to_string();
-    let R = "R".to_string();
-    for line_iter in &lines {
-        let line = pstr::rstrip(&line_iter);
-        let line_chars = line.chars().collect::<Vec<_>>();
-        let direction = line_chars[0].to_string();
-        let increment = int::<i32>(&(line_chars[1..].iter().collect::<String>()));
-        if direction == R {
-            current += increment;
-        } else if direction == L {
-            current -= increment;
-        } else {
-            panic!("Unknown direction");
-        }
-        current = current % 100;
-        if current < 0 {
-            // mod in rust i32 keeps the negative reminder
-            current += 100;
-        }
-        if current == 0 {
-            total += 1;
-        }
-        // zero crossings
-        if direction == R && current < (increment % 100) && current != 0 {
-            total += 1;
-        }
-        if direction == L && current > 100 - (increment % 100) {
-            total += 1;
-        }
-        // full loops
-        total += increment / 100;
+pub fn main_2() -> u64 {
+    {
+        open("day02/input.txt", "r")
+        .readlines()[0]
+        .split(",")
+        .map(
+            |x| x.split("-")
+            .collect::<Vec<&str>>().into_iter()
+            // or lstrip 0
+            .map(int::<u64>)
+            .collect::<Vec<_>>()
+        )
+        .collect::<Vec<_>>().into_iter()
+        .map(|pair| {
+            let mut subtotal = 0;
+            for c in pair[0]..pair[1] + 1 {
+                let c_string = c.to_string();
+                let c_chars = c_string.chars();
+                let c_chars_len = c_chars.clone().count();
+                for half in 1..c_chars_len / 2 + 1 {
+                    if c_chars_len % half != 0 {
+                        // now I need to check for "even" len
+                        continue;
+                    }
+                    let c_chars_vec = c_chars.clone().collect::<Vec<char>>();
+                    if c_chars_vec[..half].iter().cycle().take(c_chars_len).copied().collect::<Vec<_>>() == c_chars_vec {
+                        subtotal += c;
+                        break;
+                    }
+                }
+            }
+            subtotal
+        })
+        .collect::<Vec<_>>().iter()
+        .sum()
     }
-
-    total
 }

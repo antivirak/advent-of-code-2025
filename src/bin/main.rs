43254c29f68ca73a1700c6ca11004ec6c2fe9f3f
cerman <jaroslav.cerman@msd.com>
2025-12-13 01:03:55 +0100

    Rm junk file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -15,6 +15,14 @@ fn main() {
     // println!("{:?}", advent_25_06::main_2());
     // println!("{:?}", advent_25_07::main_1());
     // println!("{:?}", advent_25_07::main_2());
-    println!("{:?}", advent_25_08::main_1());
-    println!("{:?}", advent_25_08::main_2());
+    // println!("{:?}", advent_25_08::main_1());
+    // println!("{:?}", advent_25_08::main_2());
+    // println!("{:?}", advent_25_09::main_1());
+    // println!("{:?}", advent_25_09::main_2());
+    // println!("{:?}", advent_25_10::main_1());
+    // println!("{:?}", advent_25_10::main_2());
+    // println!("{:?}", advent_25_11::main_1());
+    // println!("{:?}", advent_25_11::main_2());
+    println!("{:?}", advent_25_12::main_1());
+    println!("{:?}", advent_25_12::main_2());
 }
